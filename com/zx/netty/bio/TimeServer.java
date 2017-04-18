71644ac3348499283f92f6c81cd97b0d025e4f12
NearXdu <as272434721@163.com>
2017-04-18 20:57:30 +0800

    first commit

---
+++ com/zx/netty/bio/TimeServer.java
@@ -0,0 +1,38 @@
+package com.zx.netty.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by zhangxiao on 17-4-18.
+ */
+public class TimeServer {
+    public static void main(String[] args) throws IOException {
+        int port = 1024;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+
+            }//end try catch module
+        }//end if
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server is start in port : " + port);
+            Socket socket = null;
+            while (true) {
+                socket = server.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+
+        } finally {
+            if (server!=null){
+                System.out.println("The time server close");
+                server.close();
+                server =null;
+            }
+        }
+    }
+}
---
+++ hello.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zhangxiao on 17-4-18.
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
